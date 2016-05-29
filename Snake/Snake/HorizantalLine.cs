d94d4e00e670d79d8ee18453ec524f6f3e81b64d
posttj <posttj@yandex.ru>
2016-05-29 14:29:06 +0500

    new class with horizantalLine name

---
+++ Snake/Snake/HorizantalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizantalLine
+    {
+        private readonly List<Point> _pList;
+
+        public HorizantalLine(int xLeft, int xRight, int y, char sym)
+        {
+            _pList = new List<Point>();
+            for (var x = xLeft; x < xRight; x++)
+            {
+                var p = new Point(x, y, sym);
+                _pList.Add(p);
+            }
+        }
+
+        public void Drow()
+        {
+            foreach (var point in _pList)
+            {
+                point.Draw();
+            }
+        }
+    }
+}
