62ff298072567d1ecc3c0852baa3b4cd4901060a
posttj <posttj@yandex.ru>
2016-05-29 14:59:27 +0500

    new class figure (Фигура) создаем новый класс и наследуемся от него

---
+++ Snake/Snake/Figure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Snake
+{
+    // Фигура
+    class Figure
+    {
+        protected List<Point> PList;
+ 
+        public void Drow()
+        {
+            foreach (var point in PList)
+            {
+                point.Draw();
+            }
+        }
+    }
+}
