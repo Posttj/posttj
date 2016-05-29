e4abb3f18bd54974d642eeb60ae4f9402ca158d0
posttj <posttj@yandex.ru>
2016-05-29 15:04:02 +0500

    Новый класс вертикальной линии

---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Snake
+{
+    class VerticalLine : Figure
+    {
+        //private readonly List<Point> _pList;
+
+        public VerticalLine(int yUp, int yDown, int x, char sym)
+        {
+            PList = new List<Point>();
+
+            for (var y = yUp; y < yDown; y++)
+            {
+                var p = new Point(x, y, sym);
+                PList.Add(p);
+            }
+        }
+
+        //public void Drow()
+        //{
+        //    foreach (var point in _pList)
+        //    {
+        //        point.Draw();
+        //    }
+        //}
+    }
+}
