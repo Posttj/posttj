5d2a576ad247d56ed9dea109a1a216dc3b2870a0
posttj <posttj@yandex.ru>
2016-05-29 14:05:49 +0500

    my point class

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Snake
+{
+    internal class Point
+    {
+        private readonly int _x;
+        private readonly int _y;
+        private readonly char _sym;
+
+        public Point(int x, int y, char sym)
+        {
+            _x = x;
+            _y = y;
+            _sym = sym;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(_x, _y);
+            Console.WriteLine(_sym);
+        }
+    }
+}
